0ff30e069d3c89ced44e2ec4d5a67352aab3fff3
RevathiGollu <116713328+RevathiGollu@users.noreply.github.com>
2025-01-09 11:45:56 +0530

    Update apiService.js

--- src/Task27/apiService.js
+++ src/Task27/apiService.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-
-// API call with retry logic
 export const fetchData = async (url, retries = 3, delay = 1000) => {
   for (let i = 0; i < retries; i++) {
     try {
@@ -9,7 +7,7 @@ export const fetchData = async (url, retries = 3, delay = 1000) => {
     } catch (error) {
       console.error(`Attempt ${i + 1} failed:`, error.message);
       if (i < retries - 1) {
-        await new Promise((res) => setTimeout(res, delay)); // Wait before retrying
+        await new Promise((res) => setTimeout(res, delay)); 
       } else {
         throw error;
       }
