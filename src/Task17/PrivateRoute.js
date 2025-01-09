968e75261d5731599767975da723778d604ab451
RevathiGollu <116713328+RevathiGollu@users.noreply.github.com>
2025-01-09 11:22:57 +0530

    Update PrivateRoute.js

--- src/Task17/PrivateRoute.js
+++ src/Task17/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom'; // Use Navigate for redirect in v6
+import { Route, Navigate } from 'react-router-dom'; 
 import { useAuth } from '../Context1.js/AuthContext';
 
 const PrivateRoute = ({ element: Element, ...rest }) => {
@@ -8,7 +8,7 @@ const PrivateRoute = ({ element: Element, ...rest }) => {
   return (
     <Route
       {...rest}
-      element={user ? <Element /> : <Navigate to="/login" />}  // Render Element or Redirect
+      element={user ? <Element /> : <Navigate to="/login" />}  
     />
   );
 };
